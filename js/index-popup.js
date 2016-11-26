e5f295f70aa90ac279280bc78324f9ca1959f978
gormax <m.kandid@gmail.com>
2016-11-26 03:57:37 +0300

    -

--- js/index-popup.js
+++ js/index-popup.js
@@ -1,27 +1,27 @@
 
 var link = document.querySelector(".contact-btn");
-var popup-background = document.querySelector(".feedback-background");
-var popup = popup-background.querySelector(".feedback");
+var popup_background = document.querySelector(".feedback-background");
+var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".close-btn");
 var login = popup.querySelector(".name");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
-  popup-background.classList.add("popup-window");
+  popup_background.classList.add("background-visible");
   popup.classList.add("popup-window");
   login.focus();
 });
 
 close.addEventListener("click", function(event) {
   event.preventDefault();
-  popup-background.classList.remove("popup-window");
+  popup_background.classList.remove("background-visible");
   popup.classList.remove("popup-window");
 });
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (popup.classList.contains("popup-window")) {
-      popup-background.classList.remove("popup-window");
+      popup_background.classList.remove("background-visible");
       popup.classList.remove("popup-window");
     }
   }
